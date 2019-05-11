db16912ac388d346f904c77d18394e5d9efb0306
Abhishek S <31563604+abhisassy@users.noreply.github.com>
2019-05-12 01:37:38 +0530

    Add files via upload

--- src/implementation.cpp
+++ src/implementation.cpp
@@ -26,9 +26,10 @@ tree<T_datatype, T_predicate>::tree(node<T_datatype>* param):root(param){
 template<typename T_datatype, typename T_predicate>
 tree<T_datatype, T_predicate>::tree(T_datatype val){
 	root=new node<T_datatype>;
-	root->value=val;
-	root->left=nullptr;
-	root->right=nullptr;
+	root->value  = val;
+	root->left   = nullptr;
+	root->right  = nullptr;
+	root->parent = nullptr;
 	size=1;
 	cout << "Parameterized constructor\n";	
 }
@@ -97,9 +98,10 @@ int tree<T_datatype, T_predicate>::num_nodes(){
 template<typename T_datatype, typename T_predicate>
 void tree<T_datatype, T_predicate>::insert(T_datatype element){
 	node<T_datatype>* ele=new node<T_datatype>;
-	ele->value=element;
-	ele->left=nullptr;
-	ele->right=nullptr;
+	ele->value  = element;
+	ele->left   = nullptr;
+	ele->right  = nullptr;
+	ele->parent = nullptr;
 	T_predicate fn = T_predicate();
 	node<T_datatype>* iter=root, *prev=nullptr;
 	if(root==nullptr){
@@ -111,6 +113,7 @@ void tree<T_datatype, T_predicate>::insert(T_datatype element){
 		if(fn(element,iter->value)){
 			if(iter->left==nullptr){
 				iter->left=ele;
+				ele->parent=iter;
 				break;
 			}
 			else
@@ -119,6 +122,7 @@ void tree<T_datatype, T_predicate>::insert(T_datatype element){
 		else{
 			if(iter->right==nullptr){
 				iter->right=ele;
+				ele->parent=iter;
 				break;
 			}
 			else if(iter->right->value==element || iter->value==element)
@@ -128,4 +132,140 @@ void tree<T_datatype, T_predicate>::insert(T_datatype element){
 		}
 	}
 	size++;
+}
+
+
+template<typename T_datatype, typename T_predicate>
+bool tree<T_datatype, T_predicate>::find(T_datatype element){
+
+	node<T_datatype>* iter = find_(element);
+	if(iter == nullptr)
+		return false;
+	else 
+		return true;
+
+}
+template<typename T_datatype, typename T_predicate>
+node<T_datatype>*  tree<T_datatype, T_predicate>::find_(T_datatype element){
+	node<T_datatype>* iter=root;
+	
+	T_predicate fn = T_predicate();
+	
+	while(true){
+		if(iter == nullptr)
+			return nullptr;
+		if(iter->value == element)
+			return iter;
+		if(fn(element,iter->value)){
+			iter = iter->left;
+		}
+		else
+			iter = iter->right;
+	}
+}
+
+template<typename T_datatype, typename T_predicate>
+const T_datatype* tree<T_datatype, T_predicate>::parent(T_datatype element){
+	
+	node<T_datatype>* iter  = find_(element);
+	
+	if(iter == nullptr || iter->parent==nullptr)
+		return nullptr;
+	else	
+		return &iter->parent->value;	
+}
+
+template<typename T_datatype, typename T_predicate>
+const T_datatype* tree<T_datatype, T_predicate>::left(T_datatype element){
+	
+	node<T_datatype>* iter  = find_(element);
+	
+	if(iter == nullptr)
+		return nullptr;
+	if(iter->left!=nullptr)
+		return &iter->left->value;
+	else
+		return nullptr;
+	
+}
+
+template<typename T_datatype, typename T_predicate>
+const T_datatype* tree<T_datatype, T_predicate>::right(T_datatype element){
+	
+	node<T_datatype>* iter  = find_(element);
+	
+	if(iter == nullptr)
+		return nullptr;
+	if(iter->right!=nullptr)
+		return &iter->right->value;
+	else
+		return nullptr;
+	
+}
+template<typename T_datatype, typename T_predicate>
+bool tree<T_datatype, T_predicate>::erase(T_datatype element){
+
+	node<T_datatype>* iter  = find_(element);
+	if(iter == nullptr)
+		return false;
+	if(iter->left == nullptr && iter->right == nullptr)
+		{
+			if(iter->value == root->value)
+				root = nullptr;
+			else{
+				const T_datatype *temp = left(iter->parent->value);
+				if(temp!=nullptr  && *temp==iter->value)
+					iter->parent->left=nullptr;
+				else
+					iter->parent->right=nullptr;
+			}
+			delete iter;
+			iter = nullptr;
+		}	
+	
+	else if(iter->left == nullptr){
+		if(iter->value == root->value){
+			root = root->right;
+		}
+		else{
+			const T_datatype *temp = left(iter->parent->value);
+			if(temp!=nullptr  && *temp==iter->value)
+				iter->parent->left=iter->right;
+			else
+				iter->parent->right=iter->right;
+		}
+		delete iter;
+		iter = nullptr;  
+		
+	}
+	else if(iter->right == nullptr){
+		if(iter->value == root->value){
+			root = root->left;
+		}
+		else{
+			const T_datatype *temp = left(iter->parent->value);
+			if(temp!=nullptr  && *temp==iter->value)
+				iter->parent->left=iter->left;
+			else
+				iter->parent->right=iter->left;
+		}
+		delete iter;
+		iter = nullptr;  
+		
+		
+	}
+	else{
+		
+		node<T_datatype>* iter2 = iter;
+		iter = iter->right;
+		while(iter->left!=nullptr){
+			iter = iter->left;
+		}
+		iter->parent->left=nullptr;
+		iter2->value = iter->value;
+		delete iter;
+		iter = nullptr;  
+	}
+	size--;
+	return true;
 }
